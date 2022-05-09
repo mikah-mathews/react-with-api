3f389a31ed452d8b85f7ca2c618afd2fb3f92669
Mikah Mathews <mikah@outofband.org>
2022-05-09 16:54:16 -0700

    Code through lesson 6 - Async actions with Redux thunk

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -1,24 +1,5 @@
 import React from 'react';
 
-
-makeApiCall = () => {
-  fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-  .then(response => response.json())
-  .then(
-    (jsonifiedResponse) => {
-      this.setState({
-        isLoaded: true,
-        headlines: jsonifiedResponse.results
-      });
-    })
-    .catch((error) => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    });
-}
-
 componentDidMount() {
   this.makeApiCall()
 }
